d600a51c2bb17db04b82c771b3cab7cbe9dad23a
Valeriy Manenkov <v.manenkov@gmail.com>
2018-07-07 20:26:17 +0300

    Fix tests

--- test/naive_bayes/core_test.clj
+++ test/naive_bayes/core_test.clj
@@ -2,6 +2,38 @@
   (:require [clojure.test :refer :all]
             [naive-bayes.core :refer :all]))
 
+(defn long-str [& strings]
+  (clojure.string/join "\n" strings))
+
+(def data
+  (long-str
+    "Иван Мужской"
+    "Пётр Мужской"
+    "Валерий Мужской"
+    "Александр Мужской"
+    "Алёна Женский"
+    "Мария Женский"
+    "Маргарита Женский"
+    "Александра Женский"))
+
+(def samples
+  (->>
+    (clojure.string/split-lines data)
+    (map #(clojure.string/split % #"\s"))))
+
+(defn get-features [sample]
+  {:last-letter (last sample)
+   :first-letter (first sample)
+   :second-letter (second sample)})
+
+(def features
+  (->> samples
+    (map (fn [w] [(get-features (first w)) (second w)]))))
+
+(def classifier
+  (train features))
+
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= (classify classifier (get-features "Агафья")))
+           "Женский")))
