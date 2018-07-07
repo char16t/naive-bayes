090ae9b34a302eec3c956581d7c74a9820ca119d
Valeriy Manenkov <v.manenkov@gmail.com>
2018-07-07 20:12:24 +0300

    Update meta: project description and url

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject naive-bayes "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Naive Bayes classifier"
+  :url "https://github.com/char16t/naive-bayes"
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
