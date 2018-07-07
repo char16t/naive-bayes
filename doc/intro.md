2dd38cee1d282ae0047b5ec9486f95699586b8d1
Valeriy Manenkov <v.manenkov@gmail.com>
2018-07-07 19:43:47 +0300

    Initial import (new lein project)

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to naive-bayes
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/naive_bayes/core_test.clj
@@ -0,0 +1,7 @@
+(ns naive-bayes.core-test
+  (:require [clojure.test :refer :all]
+            [naive-bayes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
